7feb1bd146c290ac60e1bef54cab0eb71094b984
Anil Yurtsever <ianilyurtsever@gmail.com>
2020-05-27 12:57:03 +0300

    Angular project updates

--- expenseAppFrontend/e2e/src/app.e2e-spec.ts
+++ expenseAppFrontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('expenseAppFrontend app is running!');
+    expect(page.getTitleText()).toEqual('expense-app-frontend app is running!');
   });
 
   afterEach(async () => {
